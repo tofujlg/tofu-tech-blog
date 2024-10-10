fbbfdc98963444bd6a8b243f609be492b7d491e4
Kohei Okumura <tofuredbull@gmail.com>
2024-10-10 09:59:52 +0900

    Add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,42 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { siteConfig } from "@/config/site";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Me",
+  description: "Information about me",
+};
+
+export default async function AboutPage() {
+  return (
+    <div className="container max-w-6xl py-6 lg:py-10">
+      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
+        <div className="flex-1 space-x-4">
+          <h1 className="inline-block font-black text-4xl lg:text-5xl">
+            About Me
+          </h1>
+        </div>
+      </div>
+      <hr className="my-8" />
+      <div className="flex flex-col md:flex-row gap-8 items-center md:items-start">
+        <div className="min-w-48 max-w-48 flex flex-col gap-2">
+          <Avatar className="h-48 w-48">
+            <AvatarImage src="/avatar.jpeg" alt={siteConfig.author} />  
+            <AvatarFallback>JC</AvatarFallback>
+          </Avatar>
+          <h2 className="text-2xl font-bold text-center break-words">
+            {siteConfig.author}
+          </h2>
+          <p className="text-muted-foreground text-center break-words">
+            Software Developer
+          </p>
+        </div>
+        <p className="text-muted-foreground text-lg py-4">
+            Software Developer in Tokyo.
+            Interested in all things web & mobile development.
+            Also interested in generative art.
+        </p>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,47 @@
-// import Image from "next/image";
-// import { Button } from "@/components/ui/button"
+import { buttonVariants } from "@/components/ui/button";
+// import { siteConfig } from "@/config/site";
+import { cn, sortPosts } from "@/lib/utils";
+import { posts } from "#site/content";
+import Link from "next/link";
+import { PostItem } from "@/components/post-item";
 
 export default function Home() {
-  return <section>Hello</section>
+  const latestPosts = sortPosts(posts).slice(0, 5);
+  return (
+    <>
+      <section className="pt-6 md:pb-12 md:mt-10 ">
+        <div className="container flex flex-col gap-4 text-center">
+          <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
+            Welcome to tofujlg&apos;s tech blog.
+          </p>
+        </div>
+      </section>
+      <section className="container max-w-4xl flex flex-col">
+        {/* <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-center">
+          Latest Posts
+        </h2> */}
+        <ul className="flex flex-col">
+          {latestPosts.map((post) => (
+              <li key={post.slug} className="first:border-t first:border-border">
+                <PostItem
+                  slug={post.slug}
+                  title={post.title}
+                  description={post.description}
+                  date={post.date}
+                  tags={post.tags}
+                />
+              </li>
+          ))}
+        </ul>
+          <div className="flex flex-col gap-4 justify-center sm:flex-row pt-6 pb-6">
+            <Link
+              href="/blog"
+              className={cn(buttonVariants({ size: "lg" }), "w-full sm:w-fit")}
+            >
+              Read more
+            </Link>
+          </div>
+      </section>
+    </>
+  );
 }
