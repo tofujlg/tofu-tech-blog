45399810c032ea141eb23d21d440e9d2f97e7be6
Kohei Okumura <tofuredbull@gmail.com>
2025-06-28 08:50:18 +0900

    feat: add new app to my works

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -19,53 +19,77 @@ export default async function WorksPage() {
       </div>
       <hr className="my-8" />
       <div className="flex flex-col md:flex-row gap-8 items-center md:items-start">
-      <section>
-        <h2 className="text-2xl font-bold">growview</h2>
+        <section>
+          <h2 className="text-2xl font-bold">growview</h2>
+          <Image
+            className="mt-4"
+            src="/growview_icon.png"
+            alt="growview"
+            width={100}
+            height={100}
+          />
+          <div className="mt-4">
+          <p className="text-sm text-muted-foreground">2024</p>
+            <p>React Native製の筋トレ記録アプリ。日々のトレーニングを記録し、成果を動画で出力することが可能。</p>
+            <p className="text-sm text-muted-foreground mt-3">Platforms :</p>
+            <p>iOS, Android</p>
+            <p className="text-sm text-muted-foreground mt-3">Tech stack :</p>
+            <p>React Native, Expo, TypeScript, NativeWind, React-Native-Reanimated, FFMpeg</p>
+            {/* <div className="flex gap-4">
+          <a href="https://astro-growview.netlify.app/">Official Site</a>
+          </div> */}
+            <div className="mt-4">
+            <p className="text-sm text-muted-foreground">Download :</p>
+            <div className="flex gap-4">
+              <p>iOS :</p>
+              <a
+                className="underline"
+                target="_blank"
+                rel="noreferrer"
+                href="https://apps.apple.com/us/app/growview/id6737449909"
+                >
+                App Store
+              </a>
+            </div>
+            <div className="flex gap-4">
+              <p>Android :</p>
+              <a
+                className="underline"
+                target="_blank"
+                rel="noreferrer"
+                href="https://play.google.com/store/apps/details?id=com.jujekebab.growview"
+                >
+                Play Store
+              </a>
+            </div>
+          </div>
+            </div>
+          </section>
+      </div>
+      <section className="mt-8">
+        <h2 className="text-2xl font-bold">Imagine</h2>
         <Image
           className="mt-4"
-          src="/growview_icon.png"
-          alt="growview"
+          src="/imagine_icon.png"
+          alt="imagine"
           width={100}
           height={100}
         />
         <div className="mt-4">
-        <p className="text-sm text-muted-foreground">2024</p>
-          <p>React Native製の筋トレ記録アプリ。日々のトレーニングを記録し、成果を動画で出力することが可能。</p>
+          <p className="text-sm text-muted-foreground">2025</p>
+          <p>目標管理アプリ</p>
           <p className="text-sm text-muted-foreground mt-3">Platforms :</p>
-          <p>iOS, Android</p>
+          <p>Android</p>
           <p className="text-sm text-muted-foreground mt-3">Tech stack :</p>
-          <p>React Native, Expo, TypeScript, NativeWind, React-Native-Reanimated, FFMpeg</p>
-          {/* <div className="flex gap-4">
-        <a href="https://astro-growview.netlify.app/">Official Site</a>
-        </div> */}
+          <p>Jetpack Compose, Kotlin</p>
           <div className="mt-4">
-          <p className="text-sm text-muted-foreground">Download :</p>
-          <div className="flex gap-4">
-            <p>iOS :</p>
-            <a
-              className="underline"
-              target="_blank"
-              rel="noreferrer"
-              href="https://apps.apple.com/us/app/growview/id6737449909"
-              >
-              App Store
-            </a>
-          </div>
-          <div className="flex gap-4">
-            <p>Android :</p>
-            <a
-              className="underline"
-              target="_blank"
-              rel="noreferrer"
-              href="https://play.google.com/store/apps/details?id=com.jujekebab.growview"
-              >
-              Play Store
-            </a>
+            <p className="text-sm text-muted-foreground">Download :</p>
+            <div className="flex gap-4">
+              <p>Android : <a className="underline" href="https://play.google.com/store/apps/details?id=com.jujekebab.imagine&pcampaignid=web_share" target="_blank" rel="noreferrer">Play Store</a></p>
+            </div>
           </div>
         </div>
-          </div>
-        </section>
-      </div>
+      </section>
     </div>
   );
 }
